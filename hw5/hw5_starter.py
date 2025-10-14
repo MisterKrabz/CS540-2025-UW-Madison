6a030f5fdb115a76f8ddd926e8923df0de32451d
Patrick Wang <pwangsuper@gmail.com>
2025-10-13 23:26:42 -0500

    added homework 5

--- hw5/hw5_starter.py
+++ hw5/hw5_starter.py
@@ -51,8 +51,8 @@ if __name__ == "__main__":
     print(weights)
     
     #Question 4 - Linear Regression w/ Gradient Descent
-    '''
-    LEARNING_RATE = 0 #IMPORTANT: You will tune this so that the gradient descent converges
+
+    LEARNING_RATE = 0.75055 #IMPORTANT: You will tune this so that the gradient descent converges
     
     gd_X = torch.tensor(X_normalized)
     gd_Y = torch.tensor(Y)
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     
     print("Q4a:")
     for iter in range(NUM_ITERS):
-        loss = #### TODO: fill this in ####
+        loss = torch.mean(((gd_X @ gd_weights) - gd_Y)**2) #### TODO: fill this in ####
         
         losses[iter] = loss.item()
         
@@ -77,10 +77,10 @@ if __name__ == "__main__":
 
         #Performs one step of gradient descent
         with torch.no_grad():
-            gd_weights -= LEARNING_RATE * gd_weights.grad
+            gd_weights -= LEARNING_RATE * gd_weights.grad # type: ignore
 
         #Resets the computation graph
-        gd_weights.grad.zero_()
+        gd_weights.grad.zero_() # type: ignore
 
     plt.figure()
     plt.plot(range(NUM_ITERS), losses)
@@ -89,21 +89,38 @@ if __name__ == "__main__":
     plt.savefig("loss_plot.jpg")
 
     print("Q4b:", LEARNING_RATE)
-    print("Q4c: *your answer here*")
-    '''
+    print("Q4c: I started with a small step size of .1 because I think that a smaller step size would be the most accurtate, but it turns out that you cant get to the bottom with a step size of .1 within 200 steps. Then gradually moved it up to .75055 where I found that the resulting biases and weights look pretty close to the biases and weights we had for Q3.")
     
     #Question 5 - Prediction
- 
-    #print("Q5: " + str(y_hat))
+    w = weights[0]
+    b = weights[1]
+    x_predict = 2024
+
+    x_tilde_predict = (x_predict - m) / (M - m)
+
+    y_hat = w * x_tilde_predict + b
+
+    print("Q5: " + str(y_hat))
 
 
     #Question 6 - Model Interpretation
-    
-    #print("Q6a: " + symbol)
-    #print("Q6b: ...")
+    w = weights[0]
+    if w > 0:
+        symbol = ">"
+    elif w < 0:
+        symbol = "<"
+    else:
+        symbol = "="
+
+    print("Q6a: " + symbol)
+    print("Q6b: if w = 0, the year has no impact on the number of days the lake was frozen.\nif w > 0 then it would indicate as the year increases, the number of frozen days tends to increase as well.\nif w < 0 then it would indicate that as the year increases, the number of frozen days tends to decrease.")
 
 
     #Question 7 - Model Limitations
-    
-    #print("Q7a: " + str(x_star))
-    #print("Q7b: ...") -
+    w = weights[0]
+    b = weights[1]
+    x_star = ((-b / w) * (M - m)) + m
+
+    print("Q7a: " + str(x_star))
+    print("Q7a: " + str(x_star))
+    print("Q7b: The prediction is not compelling since it is extrapolating wayyy beyond the range of the data, meaning that we are asssuming that the rate will remain constant, which is likely not true. There are simply too many factors we cannot account for that might affect the actual result due to how far forward it is in the future.") 
